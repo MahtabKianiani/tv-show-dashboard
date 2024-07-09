9c5fcaaff378c0007d81494030829df5429fc881
Mahtab <mahtabkianiani@yahoo.com>
2024-07-10 00:12:10 +0200

    Call API for testing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="home">
-    Home
+    Home sweet home
+    <button
+      @click="
+        {
+          {
+            testing();
+          }
+        }
+      "
+    >
+      Hello
+    </button>
   </div>
 </template>
 
 <script>
-
-
+import { getSearchShows } from "@/tvMazeService";
 export default {
-  
-}
+  methods: {
+    async testing() {
+      const x = await getSearchShows("The Godfather");
+      console.log(x.map((i) => i.show.name));
+    },
+  },
+};
 </script>
