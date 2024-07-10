355b66d727d4a1f16d35788d0e5176c3ff149941
Mahtab <mahtabkianiani@yahoo.com>
2024-07-10 18:45:44 +0200

    Add show component

---
+++ src/components/show-component.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div class="feature-card">
+      <router-link to="#">
+        <div class="card" style="width: 18rem">
+          <img class="card-img-top" :src="movie.image" alt="Card image cap" />
+          <div class="card-body">
+            <p class="card-text">{{ movie.name }}</p>
+          </div>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: {
+        name: String,
+        image: String,
+      },
+      required: true,
+    },
+  },
+};
+</script>
